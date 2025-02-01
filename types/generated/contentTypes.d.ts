7dcab755540f03865a351d2aa139ab0a200c454e
rahulb03 <122718029+rahulb03@users.noreply.github.com>
2025-02-01 16:45:41 +0530

    image type change to multiple media

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -417,7 +417,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    image: Schema.Attribute.Media<'images' | 'files'> &
+    image: Schema.Attribute.Media<'images' | 'files', true> &
       Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
